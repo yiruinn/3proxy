94ab6a7a0a978bb70faaaf149ebf20fe58ccbd0d
Yirui Zhao <zhao106@illinois.edu>
2020-12-14 21:36:02 -0800

    Update for AL2

--- 3proxyinstall.sh
+++ 3proxyinstall.sh
@@ -1,6 +1,6 @@
-version=0.8.13
-apt-get update && apt-get -y upgrade
-apt-get install gcc make git -y
+version=0.9.3
+yum update && yum -y upgrade
+yum install gcc make git -y
 wget --no-check-certificate -O 3proxy-${version}.tar.gz https://github.com/z3APA3A/3proxy/archive/${version}.tar.gz
 tar xzf 3proxy-${version}.tar.gz
 cd 3proxy-${version}
@@ -9,12 +9,13 @@ cd src
 mkdir /etc/3proxy/
 mv 3proxy /etc/3proxy/
 cd /etc/3proxy/
-wget --no-check-certificate https://github.com/SnoyIatk/3proxy/raw/master/3proxy.cfg
+wget --no-check-certificate https://github.com/yiruinn/3proxy/raw/master/3proxy.cfg
 chmod 600 /etc/3proxy/3proxy.cfg
 mkdir /var/log/3proxy/
-wget --no-check-certificate https://github.com/SnoyIatk/3proxy/raw/master/.proxyauth
+wget --no-check-certificate https://github.com/yiruinn/3proxy/raw/master/.proxyauth
 chmod 600 /etc/3proxy/.proxyauth
 cd /etc/init.d/
-wget --no-check-certificate  https://raw.github.com/SnoyIatk/3proxy/master/3proxy
+wget --no-check-certificate  https://raw.github.com/yiruinn/3proxy/master/3proxy
 chmod  +x /etc/init.d/3proxy
-update-rc.d 3proxy defaults
+chkconfig --add 3proxy
+chkconfig 3proxy on
